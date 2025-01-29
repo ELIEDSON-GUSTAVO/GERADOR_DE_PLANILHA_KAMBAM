e55a642790e9504a57f249a23eb3ff701816d988
ELIEDSON GUSTAVO <83400143+ELIEDSON-GUSTAVO@users.noreply.github.com>
2025-01-29 16:44:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let pecasSelecionadas = {}; // Objeto para armazenar o estado de seleção das p
 
 // Função para importar dados de uma planilha hospedada no GitHub
 function importarPlanilhaGitHub() {
-    const url = "https://raw.github.com/ELIEDSON-GUSTAVO/GERADOR_DE_PLANILHA_KAMBAM/blob/ec48e705b97b5048f132fcb41c386d5640f13922/BANCO%20DE%20DADOS.xlsx"; // URL RAW do arquivo no GitHub
+    const url = "https://raw.githubusercontent.com/ELIEDSON-GUSTAVO/GERADOR_DE_PLANILHA_KAMBAM/6d1dfd0cfd2f2b8a76fe6e2abac97e9bbcc6cfbf/BANCO%20DE%20DADOS.xlsx"; // URL RAW do arquivo no GitHub
 
     fetch(url)
         .then(response => response.arrayBuffer())
